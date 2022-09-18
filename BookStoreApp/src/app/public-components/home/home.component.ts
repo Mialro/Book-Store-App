59d5b150bdee8a3c9c04291f828d2ddf7cf7b14c
Tchamde Djeumo Yves <tchamdeyves@gmail.com>
2022-09-18 17:06:45 +0200

    ngDoCheck

--- BookStoreApp/src/app/public-components/home/home.component.ts
+++ BookStoreApp/src/app/public-components/home/home.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CounterService } from '../../shared/services/counter.service';
 import { AuthorsComponent } from '../../shared/components/authors/authors.component';
 import { TestService } from '../../shared/services/test.service';
+import { AuthorModel } from '../../shared/Models/author.model';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ export class HomeComponent implements OnInit {
 
   count: number = 0;
   count1: boolean = false;
+  obj : AuthorModel = {id : 23, name : "Yves"}
 
   constructor(public testService: TestService) {
     console.log("Hello from parent Constructor");
@@ -25,6 +27,7 @@ export class HomeComponent implements OnInit {
   counter(): void{
     this.count++;
     this.count1 = !this.count1
+    this.obj.id = this.count++;
   }
   
 }
--- BookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ BookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -1,19 +1,25 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AuthorModel } from '../../Models/author.model';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent implements OnInit, OnChanges {
+export class AuthorsComponent implements OnInit, OnChanges, DoCheck {
 
   @Input() myData1 : number
   @Input() myData2 : boolean
+  @Input() myData3 : AuthorModel
 
   constructor() {
     console.log("Hello from child Constructor");
     
    }
+  ngDoCheck(): void {
+    console.log(this.myData3);
+    
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
