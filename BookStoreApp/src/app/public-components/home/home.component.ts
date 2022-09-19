af15e2914fde4ce1555cb41880c8b6031557726c
Tchamde Djeumo Yves <tchamdeyves@gmail.com>
2022-09-19 10:13:08 +0200

    ngAfterContentInit VS ngAfterContentChecked and ngAfterViewInit

--- BookStoreApp/src/app/public-components/home/home.component.ts
+++ BookStoreApp/src/app/public-components/home/home.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CounterService } from '../../shared/services/counter.service';
 import { AuthorsComponent } from '../../shared/components/authors/authors.component';
 import { TestService } from '../../shared/services/test.service';
 import { AuthorModel } from '../../shared/Models/author.model';
+import { AuthorsAddressComponent } from 'src/app/shared/components/authors-address/authors-address.component';
 
 @Component({
   selector: 'app-home',
@@ -10,24 +11,33 @@ import { AuthorModel } from '../../shared/Models/author.model';
   styleUrls: ['./home.component.scss'],
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('btnCount') myTempChild : ElementRef;
   count: number = 0;
   count1: boolean = false;
   obj : AuthorModel = {id : 23, name : "Yves"}
+  address : string = "South Africa";
 
   constructor(public testService: TestService) {
     console.log("Hello from parent Constructor");
    }
 
+  ngAfterViewInit(): void {
+    console.log(this.myTempChild.nativeElement.innerHTML = "Button Updated");
+  }
+
+  
   ngOnInit(): void {
     console.log("Hello from parent ngOnInit");
+    console.log("Good " + this.myTempChild);
   }
 
   counter(): void{
     this.count++;
     this.count1 = !this.count1
     this.obj.id = this.count++;
+    this.address = this.address + this.count;
   }
   
 }
--- BookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ BookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -1,29 +1,37 @@
-import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { AuthorModel } from '../../Models/author.model';
+import { AuthorsAddressComponent } from '../authors-address/authors-address.component';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent implements OnInit, OnChanges, DoCheck {
+export class AuthorsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
 
   @Input() myData1 : number
   @Input() myData2 : boolean
   @Input() myData3 : AuthorModel
+  @ContentChild(AuthorsAddressComponent) authorsAddress : AuthorsAddressComponent
 
   constructor() {
     console.log("Hello from child Constructor");
     
    }
+  ngAfterContentInit(): void {
+    console.log("Hello AfterContentInit: " + this.authorsAddress?.address);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("Hello AfterContentChecked: " + this.authorsAddress?.address);
+  }
+
   ngDoCheck(): void {
     console.log(this.myData3);
-    
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
-    
   }
 
   ngOnInit(): void {
